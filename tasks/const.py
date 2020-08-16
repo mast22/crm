ff340c4b6fd5b900f1c2774fcaf4bdd4113584e5
Kozhenin Nick <kozhenin.nick@gmail.com>
2020-08-16 18:55:33 +0300

    Added todo

--- tasks/const.py
+++ tasks/const.py
@@ -8,14 +8,27 @@ class TaskStatuses:
     QUESTIONED = 'questioned'
     COMPLETED = 'completed'
 
+# Тимлид - на оценкеВходящая, в работе
+#
+# На оценке - созданные ПМами заявки выбираются тим лидами
+# В работе - заявки, которые выполнят данный Тимлид
+#
+# ПМ - входящие, в работе
+#
+# Входящие - созданные заявки, которые не были отданы в работу
+# В работе - заявки, отданные в работе
 
-TASK_STATUS_CHOICES = (
-    (TaskStatuses.NEW, _('Новая')),
-    (TaskStatuses.QUESTIONED, _('Есть вопросы')),
-    (TaskStatuses.RATED, _('Оцененная')),
-    (TaskStatuses.REJECTED, _('Отклоненная')),
-    (TaskStatuses.IN_PROGRESS, _('В работе')),
-    (TaskStatuses.COMPLETED, _('Завершена')),
+# Вопросы:
+# 1. Если все отклоняют заявку, то куда она попадает
+# 2. Если заявка завершена, то куда она попадает
+
+TASK_STATUS_CHOICES = (                             #   Проджект манагер    Тимлид
+    (TaskStatuses.NEW, _('Новая')),                 #   Входящие                            На оценке
+    (TaskStatuses.QUESTIONED, _('Есть вопросы')),   #   Входящая                            На оценке
+    (TaskStatuses.RATED, _('Оцененная')),           #   Входящая                            На оценке
+    (TaskStatuses.REJECTED, _('Отклоненная')),      #   Входящая                            На оценке
+    (TaskStatuses.IN_PROGRESS, _('В работе')),      #   В работе                            В работе
+    (TaskStatuses.COMPLETED, _('Завершена')),       #   В работе                            В работе
 )
 
 
